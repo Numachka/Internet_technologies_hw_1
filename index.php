c59eb890a3baa494f5889b24db8085e394f2f0d8
Numachka <nahumkletkin@gmail.com>
2022-02-18 11:56:26 +0200

    Small fixes in paragraphs. Increased mapping coords for easier clicking.

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 <h1>Top 5 Visited Locations!</h1>
 <p>Click on any of the blue circles to learn more about that location!</p>
 <map name="wordlmap">
-    <area shape="rect" coords="162,212,166,216" href="place_pages/place_one.php" alt="worldoutline.jpg">
-    <area shape="rect"  coords="159,221,163,225" href="place_pages/place_two.php" alt="worldoutline.jpg">
-    <area shape="rect"  coords="296,156,302,160" href="place_pages/place_three.php" alt="worldoutline.jpg">
-    <area shape="rect"  coords="352,172,356,176" href="place_pages/place_four.php" alt="worldoutline.jpg">
-    <area shape="rect"  coords="309,172,313,176" href="place_pages/place_five.php" alt="worldoutline.jpg">
+    <area shape="rect" coords="161,211,167,217" href="place_pages/place_one.php" alt="worldoutline.jpg">
+    <area shape="rect"  coords="158,220,164,226" href="place_pages/place_two.php" alt="worldoutline.jpg">
+    <area shape="rect"  coords="295,155,303,161" href="place_pages/place_three.php" alt="worldoutline.jpg">
+    <area shape="rect"  coords="351,171,357,177" href="place_pages/place_four.php" alt="worldoutline.jpg">
+    <area shape="rect"  coords="308,171,314,177" href="place_pages/place_five.php" alt="worldoutline.jpg">
 </map>
 <img class="worldmap" usemap="#wordlmap" src="place_pages/images/worldoutline.jpg" alt="worldoutline.jpg">
 
